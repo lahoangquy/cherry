26f97ecfdb2656350b8817ba3e45f65977a7eed1
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2021-04-14 08:17:19 +0930

    Add files via upload

---
+++ build/precache-manifest.35af9c3c437744f9f8fc327be6388726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed81f34e744cb07331c40ae85129e03",
+    "url": "/cherry/index.html"
+  },
+  {
+    "revision": "b6fe57f70cc225a1d3cd",
+    "url": "/cherry/static/css/main.0838601a.chunk.css"
+  },
+  {
+    "revision": "7072d8530a1b196c4364",
+    "url": "/cherry/static/js/2.978dd9bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cherry/static/js/2.978dd9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fe57f70cc225a1d3cd",
+    "url": "/cherry/static/js/main.1e419801.chunk.js"
+  },
+  {
+    "revision": "4eace69ed27f6d828452",
+    "url": "/cherry/static/js/runtime-main.258260f5.js"
+  }
+]);
